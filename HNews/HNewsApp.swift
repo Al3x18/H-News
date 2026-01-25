1fd91382fb391199b2be9fb2827fbbfde4857311
Alex De Pasquale <alexdepasquale1997@gmail.com>
2026-01-25 17:21:05 +0100

    Initial Commit

---
+++ HNews/HNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNewsApp.swift
+//  HNews
+//
+//  Created by Alex De Pasquale on 25/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
