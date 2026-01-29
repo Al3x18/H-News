47b5184956809e2e77d2e310226e65cdef352ce8
Alex De Pasquale <alexdepasquale1997@gmail.com>
2026-01-29 21:41:34 +0100

    Favorites model for SwiftData persistence

---
+++ HNews/View/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  HNews
+//
+//  Created by Alex De Pasquale on 29/01/26.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
